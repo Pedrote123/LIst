8d5c60ce698e0de06e00fc2b8f6fbdcce6cd6cbb
Pedrote123 <[200~pedrocovetta123@gmail.com~>
2024-07-03 17:20:07 -0300

    Help page responsiveness improved and Sign in and Sign up pages created

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
         window.location.href = 'Sub-Pages/help.html';
     });
 
+    document.querySelectorAll('#Header__MainDisplay--Container_Buttons li')[0].addEventListener('click', ()=>{
+        window.location.href = 'Sub-Pages/SignIn.html';
+    })
+
+    document.querySelectorAll('#Header__MainDisplay--Container_Buttons li')[1].addEventListener('click', ()=>{
+        window.location.href = 'Sub-Pages/SignIn.html';
+    })
+
     var notMainPageLogo = document.querySelectorAll(".Header__NavBar--List_Title.notMainPage");
 
     for (let i = 0; i < notMainPageLogo.length; i++){
