fe1f36dc9f7898308e210bfb523a92a96f7783ce
Pedrote123 <[200~pedrocovetta123@gmail.com~>
2024-07-13 21:11:47 -0300

    NavBar functioning

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,22 +1,10 @@
 document.addEventListener('DOMContentLoaded', ()=>{ 
 
-    document.querySelector('.Header__NavBar--List_Help').addEventListener('click', ()=>{
-        window.location.href = 'Sub-Pages/help.html';
-    });
-
-    document.querySelectorAll('#Header__MainDisplay--Container_Buttons li')[0].addEventListener('click', ()=>{
+    document.querySelectorAll('.Header__MainDisplay--Container_Buttons li')[0].addEventListener('click', ()=>{
         window.location.href = 'Sub-Pages/SignIn.html';
     })
 
-    document.querySelectorAll('#Header__MainDisplay--Container_Buttons li')[1].addEventListener('click', ()=>{
+    document.querySelectorAll('.Header__MainDisplay--Container_Buttons li')[1].addEventListener('click', ()=>{
         window.location.href = 'Sub-Pages/SignIn.html';
     })
-
-    var notMainPageLogo = document.querySelectorAll(".Header__NavBar--List_Title.notMainPage");
-
-    for (let i = 0; i < notMainPageLogo.length; i++){
-        notMainPageLogo[i].addEventListener('click', ()=>{
-            window.location.href = '../index.html'
-        });
-    }
 })
